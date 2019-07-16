76c4d8f0ba0c88ad5a90319baff95f647e7caccf
Arnaud POINTET <arnaud.pointet@gmail.com>
2019-07-16 14:26:01 +0200

    viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,20 @@ export default {
     head: {
         title: process.env.npm_package_name || '',
         meta: [
-          { charset: 'utf-8' },
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
           { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
         ],
         link: []
     },
     icon: {
         iconFileName: 'favicon.png'
+    },
+    manifest: {
+        name: 'Oipnet Portfolio',
+        lang: 'fr'
+    },
+    meta: {
+        viewport: 'width=device-width, initial-scale=1',
+        charset: 'utf-8'
     },
     modules: [
         '@nuxtjs/axios',
