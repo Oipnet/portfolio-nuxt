53c2c1f57496bda074988b86616852f3cc2b6332
Arnaud POINTET <arnaud.pointet@gmail.com>
2019-07-18 17:40:21 +0200

    temp fix

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 export default (store) => {
-    const socket = io(process.env.SOCKET_HOST_URL || "http://localhost:6666")
+    const socket = io(process.env.SOCKET_HOST_URL || "https://porfolio-bot.herokuapp.com:6666")
 
     socket.on('answer', (message) => {
         store.commit('chatbox/add', message)
