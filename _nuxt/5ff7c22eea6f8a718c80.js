d1c6ebb8210b283a5da0ff82bc1248cbda2e54dc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-19 14:21:26 +0000

    Deploy Oipnet/portfolio-nuxt to github.com/Oipnet/portfolio-nuxt.git:gh-pages

---
+++ _nuxt/5ff7c22eea6f8a718c80.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{251:function(t,n,e){"use strict";e.r(n);var o=e(16),component=Object(o.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("h1",[this._v("Contact "),n("NuxtLink",{attrs:{to:"/"}},[this._v("Go home")])],1)},[],!1,null,null,null);n.default=component.exports}}]);
