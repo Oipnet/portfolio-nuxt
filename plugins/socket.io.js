c350d98575bc02095baf1bf65799dddf82611a15
Arnaud POINTET <arnaud.pointet@gmail.com>
2019-07-19 16:18:44 +0200

    socket

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 export default (store) => {
-    const socket = io(process.env.SOCKET_HOST_URL || "http://localhost:9000")
+    const socket = io(process.env.SOCKET_HOST_URL || "http://localhost:9999")
 
     socket.on('answer', (message) => {
         store.commit('chatbox/add', message)
